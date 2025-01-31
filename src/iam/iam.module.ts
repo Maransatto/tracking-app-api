03bbc0f9fbb1fa0c6181a03648fc82ed38b03d76
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-31 20:04:48 -0300

    feat(Authentication): :sparkles: Protecting our routes with a Guard

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
+import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
+import jwtConfig from './config/jwt.config';
 import { BcryptService } from './hashing/bcrypt.service';
 import { HashingService } from './hashing/hashing.service';
-import { JwtModule } from '@nestjs/jwt';
-import jwtConfig from './config/jwt.config';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -19,6 +21,11 @@ import { ConfigModule } from '@nestjs/config';
     {
       provide: HashingService,
       useClass: BcryptService,
+    },
+    // the following will ensure that every route is protected by the access token guard by default
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
     },
     AuthenticationService,
   ],
