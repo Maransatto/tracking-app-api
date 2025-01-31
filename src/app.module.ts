548d1955953579472ad19dd25289c5ebd3c731a0
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2025-01-31 17:38:38 -0300

    feat: :sparkles: Implementing Hashing Passwords

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
 import { UsersModule } from './users/users.module';
+import { IamModule } from './iam/iam.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { UsersModule } from './users/users.module';
     ClientsModule,
     ConfigModule,
     UsersModule,
+    IamModule,
   ],
   controllers: [AppController],
   providers: [AppService],
